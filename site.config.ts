e03821ce63cccba8c5cc93d785cde63e7972ed8d
takuchalle <me@takuchalle.dev>
2020-10-24 22:34:15 +0900

    Update

--- site.config.ts
+++ site.config.ts
@@ -1,25 +1,17 @@
 export const config = {
   siteMeta: {
-    title: "Team Blog Hub",
-    teamName: "catnose Inc.",
-    description: "RSS based blog starter kit for teams.",
+    title: "hub.takuchalle.dev",
+    teamName: "takuchalle",
+    description: "takuchalle posts hub",
   },
   siteRoot:
     process.env.NODE_ENV === "production"
-      ? "https://team-blog-hub.vercel.app"
+      ? "team-blog-hub-eight.vercel.app"
       : "http://localhost:3000",
   headerLinks: [
     {
       title: "About",
       href: "/about",
-    },
-    {
-      title: "Company",
-      href: "https://zenn.dev/catnose99/articles/cb72a73368a547756862",
-    },
-    {
-      title: "GitHub",
-      href: "https://github.com/catnose99/team-blog-hub",
     },
   ],
 };
